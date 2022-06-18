a6e1a453553f57088e9a56aa4416a4435d8de70d
Shubham Saurabh <shubhamsaurabh@outlook.com>
2022-06-18 19:06:38 +0530

    changed profile picture

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { RiNotification3Line } from 'react-icons/ri';
 import { MdKeyboardArrowDown } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-import avatar from '../data/avatar.jpg';
+import avatar5 from '../data/avatar5.jpg';
 import {Cart, Chat, Notification, UserProfile} from '.';
 import { useStateContext } from '../contexts/ContextProvider';
 
@@ -83,12 +83,12 @@ const Navbar = () => {
           <div className='flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg'
           onClick={() => handleClick('userProfile')}>
             <img 
-              src={avatar}
+              src={avatar5}
               className='rounded-full w-8 h-8'
             />
             <p>
               <span className='text-gray-400 text-14'>Hi, </span> {' '}
-              <span className='text-gray-400 font-bold ml-1 text-14'>Michael</span>
+              <span className='text-gray-400 font-bold ml-1 text-14'>Shubham</span>
             </p>
             <MdKeyboardArrowDown className='text-gray-400 text-14' />
           </div>
